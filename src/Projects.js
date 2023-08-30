f26c0b9bd32fb3ffea1f67d6ba16fdf8f5a41f8e
Sander Bell <thesanderbell@gmail.com>
2023-08-30 15:20:49 +0700

    Better Spacing in Mobile Landscape

--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 
-
 import React, { useMemo } from 'react';
-
 
 import cmairImage from './static/cmair.png';
 import rewordsImage from './static/rewords.png';
@@ -11,32 +9,37 @@ import shouldImage from './static/should.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
+function ArrowIcon() {
+  return (
+    <FontAwesomeIcon className='link-icon' icon={faArrowUpRightFromSquare} />
+  );
+}
+
 function Projects() {
-
-  const shouldImg = useMemo(() => <img draggable='false' src={shouldImage} />, []);
-  const rewordsImg = useMemo(() => <img draggable='false' src={rewordsImage} />, []);
-  const cmairImg = useMemo(() => <img draggable='false' src={cmairImage} />, []);
-
-
+  const shouldImg = useMemo(
+    () => <img draggable='false' src={shouldImage} />,
+    []
+  );
+  const rewordsImg = useMemo(
+    () => <img draggable='false' src={rewordsImage} />,
+    []
+  );
+  const cmairImg = useMemo(
+    () => <img draggable='false' src={cmairImage} />,
+    []
+  );
 
   return (
     <div id='projects'>
       <div className='proj' id='proj-one'>
-        <div className='proj-illustr'>
-          {shouldImg}
-        </div>
+        <div className='proj-illustr'>{shouldImg}</div>
         <div className='proj-descr'>
           <div className='header-with-icon'>
             <a href='https://should.today' target='_blank' rel='noreferrer'>
-              <h2 className='project-name'>
-                Should
-              </h2>
+              <h2 className='project-name'>Should</h2>
             </a>
             <a href='https://should.today' target='_blank' rel='noreferrer'>
-              <FontAwesomeIcon
-                className='link-icon'
-                icon={faArrowUpRightFromSquare}
-              />
+              <ArrowIcon />
             </a>
           </div>
           <p>
@@ -48,21 +51,14 @@ function Projects() {
         </div>
       </div>
       <div className='proj' id='proj-two'>
-        <div className='proj-illustr'>
-        {rewordsImg}
-        </div>
+        <div className='proj-illustr'>{rewordsImg}</div>
         <div className='proj-descr'>
           <div className='header-with-icon'>
             <a href='https://rewords.app' target='_blank' rel='noreferrer'>
-              <h2 className='project-name'>
-                Rewords
-              </h2>
+              <h2 className='project-name'>Rewords</h2>
             </a>
             <a href='https://rewords.app' target='_blank' rel='noreferrer'>
-              <FontAwesomeIcon
-                className='link-icon'
-                icon={faArrowUpRightFromSquare}
-              />
+              <ArrowIcon />
             </a>
           </div>
           <p>
@@ -75,21 +71,14 @@ function Projects() {
       </div>
 
       <div className='proj' id='proj-three'>
-        <div className='proj-illustr'>
-          {cmairImg}
-        </div>
+        <div className='proj-illustr'>{cmairImg}</div>
         <div className='proj-descr'>
           <div className='header-with-icon'>
             <a href='https://cmair.today' target='_blank' rel='noreferrer'>
-              <h2 className='project-name'>
-                Chiang Mai AQI
-              </h2>
+              <h2 className='project-name'>Chiang Mai AQI</h2>
             </a>
             <a href='https://cmair.today' target='_blank' rel='noreferrer'>
-              <FontAwesomeIcon
-                className='link-icon'
-                icon={faArrowUpRightFromSquare}
-              />
+              <ArrowIcon />
             </a>
           </div>
           <p>
