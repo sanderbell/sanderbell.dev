c8a878447d345b4ccca87680f1a3940008b8f1f0
Sander Bell <thesanderbell@gmail.com>
2023-10-16 12:01:32 +0700

    Updated About

--- src/About.js
+++ src/About.js
@@ -4,12 +4,13 @@ function About() {
   return (
     <div id='about'>
       <p>
-        With over 15 years of experience in communications, media management,
-        and editorial roles, I bring a wealth of expertise that aligns with the
-        profession of a web developer. Just as I meticulously curated content
-        and guided communication strategies in my editorial positions, I
-        understand the importance of creating concise code as well as seamless,
-        user-centered experiences. I would be delighted if{' '}
+        With two years of experience in programming and over 15 years in
+        communications, media management, and leading editorial roles, I bring a
+        wealth of expertise that seamlessly aligns with the profession of a web
+        developer. Just as I meticulously curated content and guided
+        communication strategies in my editorial positions, I understand the
+        importance of creating concise code as well as seamless, user-centered
+        experiences. I would be delighted if{' '}
         <a target='_blank' id='link-to-resume' href='/sander-resume.pdf'>
           my skills and knowledge
         </a>{' '}
