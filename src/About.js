f40722200bd484f1dcee738750a3a69e3cf83bd7
Sander Bell <thesanderbell@gmail.com>
2024-01-06 11:37:01 +0700

    Update about, resume.pdf

--- src/About.js
+++ src/About.js
@@ -1,19 +1,25 @@
-
 function About() {
   return (
     <div id='about'>
       <p>
-        With two years of experience in programming and over 15 years in
-        communications, media management, and leading editorial roles, I bring a
-        wealth of expertise that seamlessly aligns with the profession of a web
-        developer. Just as I meticulously curated content and guided
-        communication strategies in my editorial positions, I understand the
-        importance of creating concise code as well as seamless, user-centered
-        experiences. I would be delighted if{' '}
-        <a target='_blank' id='link-to-resume' href='/sander-resume.pdf'>
+        I'm proudly a React Native developer at{' '}
+        <a
+          target='_blank'
+          id='link-to'
+          rel='noreferrer'
+          href='https://ling-app.com'
+        >
+          Ling App
+        </a>{' '}
+        , a mobile platform for learning languages. With two years of
+        programming experience and over 15 years in communications, media
+        management, and leading editorial roles, I understand the importance of
+        creating concise code as well as seamless, user-centered experiences. I
+        would be delighted if{' '}
+        <a target='_blank' id='link-to' href='/sander-resume.pdf'>
           my skills and knowledge
         </a>{' '}
-        could help you bring your ideas and projects to fruition.
+        could help bring your ideas and projects to fruition.
       </p>
     </div>
   );
